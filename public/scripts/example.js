e9fbbebabfbad24b89e20bf40c3d44a51a230ae8
devjanaprime <dev@primeacademy.io>
2016-09-07 15:09:08 -0500

    end of lecture

--- public/scripts/example.js
+++ public/scripts/example.js
@@ -1,16 +1,27 @@
 console.log( 'example.js sourced');
 
 $( document ).ready( function(){
-  console.log( 'doc ready' );
+  console.log( 'doc ready JQ');
+
+  // on click for sendInfo id
   $( '#sendInfo' ).on( 'click', function(){
-    console.log( 'sendInfo clicked' );
-    // ajax POST to route on our server
+    console.log( 'sendInfo on click' );
+    // get user input
+    var creatureIn = $( '#creatureIn').val();
+    var noiseIn = $( '#noiseIn').val();
+    // assemble object to send to server
+    var objectToSend={
+      creature: creatureIn,
+      noise: noiseIn
+    };
+    // ajax post that sends object to /texter route
     $.ajax({
       type: 'POST',
       url: '/texter',
+      data: objectToSend,
       success: function( data ){
-        console.log( 'ajax success:', data );
-      } // end success
-    }); // end ajax
-  }) // end sendInfo on click
-}); // end doc ready
+        console.log( 'got this from server - ' + data );
+      } // end ajax success
+    });
+  }); // end sendInfo on click
+});
--- server/app.js
+++ server/app.js
@@ -1,6 +1,9 @@
 var express = require( 'express' );
 var app = express();
 var path = require( 'path' );
+var bodyParser = require( 'body-parser' );
+var urlencodedParser = bodyParser.urlencoded( { extended: false } );
+
 // spin up server
 app.listen( '3000', 'localhost', function(){
   console.log( 'server is listening on port 3000');
@@ -13,11 +16,12 @@ app.get( '/', function( req, res ){
   res.sendFile( path.resolve( 'public/index.html') );
 }); // end base url
 
-// post route receives info from client
-app.post( '/texter', function( req, res ){
-  console.log( 'texter hit' );
-  res.send( 'texter response' )
+// post URL for AJAX call
+// requires bodyParser's urlencodedparser
+app.post( '/texter', urlencodedParser, function( req, res ){
+  console.log( 'texter hit', req.body );
+  res.send( 'you sent a ' + req.body.creature );
 });
 
-// setup public folder
+// use 'public' as a static resource
 app.use( express.static( 'public' ) );
